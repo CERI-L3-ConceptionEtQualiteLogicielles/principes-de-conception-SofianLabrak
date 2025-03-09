be472dd1055b8e45154f1d00d9ccae79f665572c
SofianLabrak <sofian.labrak@gmail.com>
2025-03-09 11:25:52 +0100

    Ajout de la classe ServiceEnvoiPhoto

---
+++ src/main/java/exo1/ServiceEnvoiPhoto.java
@@ -0,0 +1,9 @@
+package exo1;
+
+import java.awt.*;
+
+public class ServiceEnvoiPhoto {
+    public String envoiePhoto(Contact c, Image image) {
+        return "Photo " + image + " Envoyer au contacte : " + c + "."
+    }
+}
