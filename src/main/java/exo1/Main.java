6f4f9946dde1543920cb8d559e6cccca3b7618d1
sofian.labrak <sofian.labrak@alumni.univ-avignon.fr>
2025-03-31 23:32:05 +0200

    Modification du main afin d'ajouté un nouveau contacte en plus. Modification de la classe ServiceEnvoiPhoto car il manquait un point virgule.

--- src/main/java/exo1/Main.java
+++ src/main/java/exo1/Main.java
@@ -3,8 +3,15 @@ package exo1;
 public class Main {
     public static void main(String[] args) {
         Contact contact1 = new ContactBuilder("Dupont", "0666666666").addPrenom("Paul").build();
+        Contact contact2 = new ContactBuilder("Labrak", "0619283045").addPrenom("Sofian").addAdresse("84 rue du CERI").addCivilite("Homme").build();
+        System.out.println("Affichage des contactes toString :");
         System.out.println(contact1);
+        System.out.println(contact2);
+        System.out.println("Affichage ddes contactes format json et xml");
         System.out.println(contact1.getInfoContact("json"));
         System.out.println(contact1.getInfoContact("xml"));
+
+        System.out.println(contact2.getInfoContact("json"));
+        System.out.println(contact2.getInfoContact("xml"));
     }
 }
--- src/main/java/exo1/ServiceEnvoiPhoto.java
+++ src/main/java/exo1/ServiceEnvoiPhoto.java
@@ -4,6 +4,6 @@ import java.awt.*;
 
 public class ServiceEnvoiPhoto {
     public String envoiePhoto(Contact c, Image image) {
-        return "Photo " + image + " Envoyer au contacte : " + c + "."
+        return "Photo " + image + " Envoyer au contacte : " + c + ".";
     }
 }
